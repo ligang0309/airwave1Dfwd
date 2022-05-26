66a3b388ef1ba29df23714141112fd48d6fd080b
Gang Li <ligang0309@ouc.edu.cn>
2022-05-26 16:08:47 +0800

    Add files via upload

---
+++ airwave1Dfwd.m
@@ -0,0 +1,107 @@
+%% PROGRAM FOR COMPUTING THE AIRWAVE 
+%  FOR 1D ISOTROPIC CONDUCTIVITY MODEL
+%   Program for computing the radial electric component of 
+%   the airwave for three-layer model with the 
+%   air half-space, seawater and seafloor half-space 
+%   for a horizontal electric dipole (HED) source.
+% 
+%  This code follows "Nordskag, J.I., Amundsen, L., 2007. 
+%   Asymptotic airwave modeling for marine controlled-source 
+%   electromagnetic surveying. Geophysics 72, F249–F255. 
+%   doi:10.1190/1.2786025. "
+%
+%  For possible questions, please contact:
+%   Gang Li
+%   Zhejiang University
+%   ligang0309@gmail.com
+%   Original codes written in 9 Jul.,2021 
+
+%% Input parameters
+% Source dipole 
+clc; clear;
+nsrc = 1;        
+
+fid = fopen('input_sources.txt', 'r');
+nsrc = fscanf(fid, '%i', 1);   % number of transmitters, 1 FOR READING A INTEGER
+[temp, count] = fscanf(fid, '%g %g %g %g %g %g', [5 nsrc]);
+temp = temp';
+
+Psrc = temp(:,1);       % moment
+PHIsrc = temp(:,2);     % azimuth (in degree)
+xsrc = temp(:,3);
+ysrc = temp(:,4);
+zsrc = temp(:,5);
+
+% Transmitting frequency
+fid = fopen('input_frequencies.txt', 'r');
+nfreq = fscanf(fid, '%i', 1);   % number of transmitters, 1 FOR READING A INTEGER
+[temp, count] = fscanf(fid, '%g', [1 nfreq]);
+temp = temp';
+
+freq = temp(:,1);    % transmitting frequency
+
+% Receivers
+fid = fopen('input_receivers.txt', 'r');
+nsite = fscanf(fid, '%i', 1);   % number of transmitters, 1 FOR READING A INTEGER
+[temp, count] = fscanf(fid, '%g %g %g %g %g %g', [6 nsite]);
+temp = temp';
+
+xsite = temp(:,1);
+ysite = temp(:,2);
+zsite = temp(:,3);
+alphasite = temp(:,4);
+betasite = temp(:,5);
+gammasite = temp(:,6);
+
+% Conductivity model (ONLY 3-LAYERED MODEL IS SUPPORTED)
+fid = fopen('input_model.txt', 'r');
+nlayer = fscanf(fid, '%i', 1);   % number of transmitters, 1 FOR READING A INTEGER
+[temp, count] = fscanf(fid, '%g %g', [2 nlayer]);
+temp = temp';
+
+depth_top = temp(:,1);
+sigma = temp(:,2);
+
+z_sea = depth_top(3);            % seawater bottom boundary depth (m), 
+                                 % note that the upper boundary is of 0 m
+sigma_sea = 1./sigma(2);         % conductivity of seawater half-space  
+sigma_seafloor = 1./sigma(3);    % conductivity of seafloor half-space 
+
+%% Calculating the airwave fields
+mu0 = 4*pi*10.^(-7);          % free-space magnetic permeability
+epsilon0 = 8.854187817*10.^(-12);   % free-space electric permittivity
+Eair = zeros(nfreq,nsrc,nsite);
+
+for ifreq = 1:nfreq
+    omega = 2*pi*freq;
+    k = sqrt(1i*omega*mu0*sigma_sea);
+    k0 = omega * sqrt(mu0*epsilon0);
+    R = (sqrt(sigma_sea) - sqrt(sigma_seafloor)) ...
+        ./ (sqrt(sigma_sea) + sqrt(sigma_seafloor));
+    
+    for isrc = 1:nsrc
+        for isite = 1:nsite
+            r_offset = (xsrc(isrc) - xsite(isite)).^2 ...
+                + (ysrc(isrc) - ysite(isite)).^2;
+            r_offset = sqrt(r_offset);
+            
+            Eair(ifreq,isrc,isite) = Psrc(isrc)*cosd(PHIsrc)*...
+                exp(1i*k*(zsite(isite)+zsrc(isrc)))*exp(1i*k0*r_offset)...
+                ./ (2*pi*sigma_sea*r_offset.^3);
+            
+            reflect1 = (1 + R*exp(2*1i*k*(z_sea-zsrc(isrc)))) ...
+                ./ (1 - R*exp(2*1i*k*z_sea));
+            
+            reflect2 = (1 + R*exp(2*1i*k*(z_sea-zsite(isite)))) ...
+                ./ (1 - R*exp(2*1i*k*z_sea));
+            
+            Eair(ifreq,isrc,isite) = Eair(ifreq,isrc,isite)...
+                *reflect1*reflect2; 
+        end             
+    end
+end
+
+% Saving the results
+save rslt.txt ysite Eair;
+
+
---
+++ readme.txt
@@ -0,0 +1,44 @@
+AIRWAVE1DFWD: Program for computing the radial electric component of the airwave for three-layer model with the air half-space, seawater and seafloor half-space for a horizontal electric dipole (HED) source.
+
+This code follows the theory given by:
+Nordskag, J.I., Amundsen, L., 2007. Asymptotic airwave modeling for marine controlled-source electromagnetic surveying. Geophysics 72, F249–F255.  doi:10.1190/1.2786025. 
+
+----------------------CODE STRUCTURE----------------------------------
+Source file:
+          --airwave1Dfwd.m: the main MATLAB file for computing the airwave fields.
+Input files:
+          --input_model.txt: the three-layered model. For example,
+                3                   ! Number of layers in this model. Currently fixed as 3.
+              -100000  1e8   ! The top depths (m) and resistivities (ohm-m).                   
+                0           0.3             
+                50         1 
+          --input_sources.txt: the file for the transmitters. For example,
+               1                    ! Number of transmitters.  
+               1 0 0 0 30       ! The dipole moment (fixed as unit), and the x,y,z depths (in m) and the azimuth (in degree) of the transmitters.
+          --input_frequencies.txt: the file for the transmitting frequencies. For example,
+               1                    ! Number of frequencies. 
+               0.25                ! The frequency values.  
+          --input_receivers.txt: the file for the receivers. For example,
+               91                   ! Number of receivers.
+               0	1000	50	0	0	0  
+               0	1100	50	0	0	0
+               0	1200	50	0	0	0
+                ... ...
+! The x,y,z depths (in m) and orientations. The orientation parameters Alpha, Beta and Gamma of sites described as follows. The receiver is horizontally rotated by an azimuth α around z-axis. It is further tilted by a dip β around x′-axis. Finally, it is rolled by γ around y′′-axis into the rotated receiver coordinates (x′′,y′′, z′′). See Li (2020) for details. Reference used: Y. Li. Finite element modeling of electromagnetic fields in two- and three-dimensional anisotropic conductivity structures (in German). PhD thesis, University of G\¨ottingen, 2000.
+
+Output files:
+          --rslt.mat: the results for the airwave fields.
+
+----------------------COPYRIGHTS----------------------------------------
+These programs may be freely redistributed under the condition that the copyright notices are not removed, and no compensation is received.  Private, research, and institutional use is free. You may distribute modified versions of this code UNDER THE CONDITION THAT THIS CODE AND ANY MODIFICATIONS MADE TO IT IN THE SAME FILE REMAIN UNDER COPYRIGHT OF THE ORIGINAL AUTHOR, BOTH SOURCE AND OBJECT CODE ARE MADE FREELY AVAILABLE WITHOUT CHARGE, AND CLEAR NOTICE IS GIVEN OF THE MODIFICATIONS. Distribution of this code as part of a commercial system is permissible ONLY BY DIRECT ARRANGEMENT WITH THE AUTHOR. 
+If you use the code, PLEASE SEND ME AN EMAIL. 
+
+Copyright 2022, Gang Li
+2022-05-26
+-----------------------------------------------------------------------------
+For possible questions, please contact:
+ 
+  Gang Li
+   Zhejiang University
+   ligang0309@gmail.com
+   Original code written in 9 Jul.,2021 
